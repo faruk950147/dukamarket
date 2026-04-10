3b530e943aed506ae42ebfa8c9cd8ce6f686a169
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-04-10 21:41:29 +0600

    I have some changes

--- config/settings.py
+++ config/settings.py
@@ -165,7 +165,7 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool, default=True)
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = config('EMAIL_PORT', cast=int, default=587)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')  # App Password production-এর জন্য
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')  # App Password production
 
 # =========================
 # Default primary key
